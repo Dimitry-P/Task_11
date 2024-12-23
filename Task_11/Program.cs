2a167bb25603dde7aab0d6737c734e7fe3f8f02a
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:31:01 +0300

    Add project files.

---
+++ Task_11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    internal class Program
+    {
+        //•	Задача 11: Напишите программу, которая выводит все числа Фибоначчи до N.
+        public static string number;
+        public static int result;
+        public static int first;
+        public static int second;
+        public static int third;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 0");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = 0; i < result; i++)
+            {
+                if (i == 0)
+                {
+                    first = 0;
+                    second = 1;
+                    Console.Write(first+", ");
+                    Console.Write(second+", ");
+                }
+               if(i != 0)
+                {
+                    third = first + second;
+                    Console.Write(third+", ");
+                    if (third == result) return;
+                    first = second;
+                    second = third;
+                }
+
+            }
+        }
+    }
+}
